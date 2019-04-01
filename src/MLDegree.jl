95cbd367dba025f5a620030f8a10653552715aa3
Sascha Timme <sascha.timme@gmail.com>
2019-04-01 17:06:47 +0200

    GaussianML

--- src/MLDegree.jl
+++ src/MLDegree.jl
@@ -1,5 +1,7 @@
 module MLDegree
 
+export GaussianML
+
 import StaticPolynomials, HomotopyContinuation
 import LinearAlgebra
 import MultivariatePolynomials
@@ -17,6 +19,8 @@ Compute with `d / d_x_{ij} log(det(X))` where `X` is a symmetric n×n matrix.
 struct LogDet
     n::Int
 end
+
+Base.size(L::LogDet) = (div(L.n*(L.n+1), 2), div(L.n*(L.n+1), 2))
 
 struct LogDetCache
     lufac::LinearAlgebra.LU{ComplexF64, Matrix{ComplexF64}}
@@ -132,20 +136,20 @@ end
 
 
 
-struct ParameterizedLogDet{P<:SP.PolynomialSystem}
+struct GaussianML{P<:SP.PolynomialSystem} <: HC.AbstractSystem
     F::P
     L::LogDet
 end
 
-function ParameterizedLogDet(f::Vector{<:MP.Abstract.PolynomialLike})
+function GaussianML(f::Vector{<:MP.AbstractPolynomialLike})
     F = SP.PolynomialSystem(f)
     m = length(f)
-    n = div(round(Int, sqrt(8m + 1) + 1), 2)
+    n = round(Int, (sqrt(8m + 1) - 1) / 2)
     L = LogDet(n)
-    ParameterizedLogDet(F, L)
+    GaussianML(F, L)
 end
 
-struct ParameterizedLogDetCache
+struct GaussianMLCache <: HC.AbstractSystemCache
     F_x::Vector{ComplexF64}
     J_F_x::Matrix{ComplexF64}
     H_F_x::Array{ComplexF64,3}
@@ -154,40 +158,54 @@ struct ParameterizedLogDetCache
     log_det_cache::LogDetCache
 end
 
-function ParameterizedLogDetCache(S::ParameterizedLogDet, x)
-    m, n = SP.npolynomials(S.F), SP.nvariables(S.F)
+function HC.cache(GML::GaussianML, x, p)
+    m, n = SP.npolynomials(GML.F), SP.nvariables(GML.F)
     F_x = zeros(ComplexF64, m)
     J_F_x = zeros(ComplexF64, m, n)
     H_F_x = zeros(ComplexF64, m, n, n)
-    log_det_cache = LogDetCache(S.L)
-    log_det_u = zeros(ComplexF64, div(m * (m - 1), 2))
-    J_log_det_u = zeros(ComplexF64, div(m * (m - 1), 2), div(m * (m - 1), 2))
-    ParameterizedLogDetCache(F_x, J_F_x, H_F_x, log_det_u, J_log_det_u, log_det_cache)
+    log_det_cache = LogDetCache(GML.L)
+    log_det_u = zeros(ComplexF64, size(GML.L)[1])
+    J_log_det_u = zeros(ComplexF64, size(GML.L)...)
+    GaussianMLCache(F_x, J_F_x, H_F_x, log_det_u, J_log_det_u, log_det_cache)
 end
 
 
-function evaluate!(u, S::ParameterizedLogDet, x, cache::ParameterizedLogDetCache)
-    SP.evaluate_and_jacobian!(cache.F_x, cache.J_F_x, S.F, x)
-    evaluate!(cache.log_det_u, S.L, cache.F_x, cache.log_det_cache)
+function HC.evaluate!(u, GML::GaussianML, x, p, cache::GaussianMLCache)
+    SP.evaluate_and_jacobian!(cache.F_x, cache.J_F_x, GML.F, x)
+    evaluate!(cache.log_det_u, GML.L, cache.F_x, cache.log_det_cache)
 
-    for i in 1:length(u)
-        u[i] = zero(ComplexF64)
-        for k = 1:size(cache.J_F_x, 1)
-            u[i] += cache.log_det_u[k] * cache.J_F_x[k, i]
+    for k in 1:length(u)
+        u[k] = zero(ComplexF64)
+        # ∇_x log(det(K(x)))
+        for i = 1:size(cache.J_F_x, 1)
+            u[k] += cache.log_det_u[i] * cache.J_F_x[i, k]
+        end
+
+        # subtract now ∇_x trace(K(x)S)
+        l = 1
+        for i=1:GML.L.n, j in i:GML.L.n
+            if i == j
+                u[k] -=  cache.J_F_x[l, k] * p[l]
+            else
+                u[k] -= 2cache.J_F_x[l, k] * p[l]
+            end
+            l += 1
         end
     end
+
     u
 end
 
 
-function jacobian!(U, S::ParameterizedLogDet, x, cache::ParameterizedLogDetCache)
-    SP.evaluate!(cache.F_x, S.F, x)
-    SP.jacobian_and_hessian!(cache.J_F_x, cache.H_F_x, S.F, x)
-    evaluate_and_jacobian!(cache.log_det_u, cache.J_log_det_u, S.L, cache.F_x, cache.log_det_cache)
+function HC.jacobian!(U, GML::GaussianML, x, p, cache::GaussianMLCache)
+    SP.evaluate!(cache.F_x, GML.F, x)
+    SP.jacobian_and_hessian!(cache.J_F_x, cache.H_F_x, GML.F, x)
+    evaluate_and_jacobian!(cache.log_det_u, cache.J_log_det_u, GML.L, cache.F_x, cache.log_det_cache)
 
     n = size(cache.J_F_x, 2)
 
     for j in 1:n, i in 1:n
+        # Hessian_x log(det(K(x)))
         U[i, j] = zero(ComplexF64)
         for k = 1:size(cache.J_F_x, 1)
             U[i, j] += cache.log_det_u[k] * cache.H_F_x[k, i, j]
@@ -199,26 +217,51 @@ function jacobian!(U, S::ParameterizedLogDet, x, cache::ParameterizedLogDetCache
                 U_i_j_k += cache.J_log_det_u[k, l] * cache.J_F_x[k, i]
             end
             U[i, j] -= U_i_j_k * cache.J_F_x[l, j]
+        end
+
+        # subtract now Hessian_x trace(K(x)S)
+
+        t = 1
+        for k=1:GML.L.n, l in k:GML.L.n
+            if k == l
+                U[i, j] -=  cache.H_F_x[t, i, j] * p[t]
+            else
+                U[i, j] -= 2cache.H_F_x[t, i, j] * p[t]
+            end
+            t += 1
         end
     end
     U
 end
 
 
-function evaluate_and_jacobian!(u, U, S::ParameterizedLogDet, x, cache::ParameterizedLogDetCache)
-    SP.evaluate!(cache.F_x, S.F, x)
-    SP.jacobian_and_hessian!(cache.J_F_x, cache.H_F_x, S.F, x)
-    evaluate_and_jacobian!(cache.log_det_u, cache.J_log_det_u, S.L, cache.F_x, cache.log_det_cache)
+function HC.evaluate_and_jacobian!(u, U, GML::GaussianML, x, p, cache::GaussianMLCache)
+    SP.evaluate!(cache.F_x, GML.F, x)
+    SP.jacobian_and_hessian!(cache.J_F_x, cache.H_F_x, GML.F, x)
+    evaluate_and_jacobian!(cache.log_det_u, cache.J_log_det_u, GML.L, cache.F_x, cache.log_det_cache)
 
     for i in 1:length(u)
+        # log(det(K(x)))
         u[i] = zero(ComplexF64)
         for k = 1:size(cache.J_F_x, 1)
             u[i] += cache.log_det_u[k] * cache.J_F_x[k, i]
+        end
+
+        # trace(K(x)S)
+        l = 1
+        for s=1:GML.L.n, t in s:GML.L.n
+            if s == t
+                u[i] -=  cache.J_F_x[l, i] * p[l]
+            else
+                u[i] -= 2cache.J_F_x[l, i] * p[l]
+            end
+            l += 1
         end
     end
 
     n = size(cache.J_F_x, 2)
     for j in 1:n, i in 1:n
+        # log(det(K(x)))
         U[i, j] = zero(ComplexF64)
         for k = 1:size(cache.J_F_x, 1)
             U[i, j] += cache.log_det_u[k] * cache.H_F_x[k, i, j]
@@ -230,6 +273,17 @@ function evaluate_and_jacobian!(u, U, S::ParameterizedLogDet, x, cache::Paramete
                 U_i_j_k += cache.J_log_det_u[k, l] * cache.J_F_x[k, i]
             end
             U[i, j] -= U_i_j_k * cache.J_F_x[l, j]
+        end
+
+        # subtract now Hessian_x trace(K(x)S)
+        t = 1
+        for k=1:GML.L.n, l in k:GML.L.n
+            if k == l
+                U[i, j] -=  cache.H_F_x[t, i, j] * p[t]
+            else
+                U[i, j] -= 2cache.H_F_x[t, i, j] * p[t]
+            end
+            t += 1
         end
     end
 
